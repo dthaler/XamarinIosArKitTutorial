ad2edb04667b0f9c5b5496dc7cdb8e9caa95ef32
Dave Thaler <dthaler@ntdev.microsoft.com>
2021-01-02 00:00:55 -0800

    Lesson 17: Corner radius
    Signed-off-by: Dave Thaler <dthaler@ntdev.microsoft.com>

--- XamarinIosApp/ViewController.cs
+++ XamarinIosApp/ViewController.cs
@@ -15,8 +15,7 @@ namespace XamarinIosApp
         {
             this.sceneView = new ARSCNView
             {
-                AutoenablesDefaultLighting = true,
-                DebugOptions = ARSCNDebugOptions.ShowWorldOrigin
+                AutoenablesDefaultLighting = true
             };
 
             this.View.AddSubview(this.sceneView);
@@ -41,25 +40,25 @@ namespace XamarinIosApp
                 WorldAlignment = ARWorldAlignment.GravityAndHeading
             }, ARSessionRunOptions.ResetTracking | ARSessionRunOptions.RemoveExistingAnchors);
 
-            // Lesson: Add image to plane
-            var width = 0.1f;
-            var length = 0.1f;
+            var size = 0.08f;
+            var colour = UIColor.Green;
+            var topRowY = 0.1f;
+            var bottomRowY = 0f;
 
-            var image = UIImage.FromFile("Images/pineapple.jpg");
+            this.sceneView.Scene.RootNode.AddChildNode(new PlaneNode(size, colour, new SCNVector3(-0.2f, topRowY, 0), 0));
+            this.sceneView.Scene.RootNode.AddChildNode(new PlaneNode(size, colour, new SCNVector3(-0.1f, topRowY, 0), 0.004f));
+            this.sceneView.Scene.RootNode.AddChildNode(new PlaneNode(size, colour, new SCNVector3(0, topRowY, 0), 0.008f));
+            this.sceneView.Scene.RootNode.AddChildNode(new PlaneNode(size, colour, new SCNVector3(0.1f, topRowY, 0), 0.012f));
+            this.sceneView.Scene.RootNode.AddChildNode(new PlaneNode(size, colour, new SCNVector3(0.2f, topRowY, 0), 0.016f));
 
-            var material = new SCNMaterial();
-            material.Diffuse.Contents = image;
-            material.DoubleSided = true;
+            this.sceneView.Scene.RootNode.AddChildNode(new PlaneNode(size, colour, new SCNVector3(-0.2f, bottomRowY, 0), 0.020f));
+            this.sceneView.Scene.RootNode.AddChildNode(new PlaneNode(size, colour, new SCNVector3(-0.1f, bottomRowY, 0), 0.024f));
+            this.sceneView.Scene.RootNode.AddChildNode(new PlaneNode(size, colour, new SCNVector3(0f, bottomRowY, 0), 0.028f));
+            this.sceneView.Scene.RootNode.AddChildNode(new PlaneNode(size, colour, new SCNVector3(0.1f, bottomRowY, 0), 0.032f));
+            this.sceneView.Scene.RootNode.AddChildNode(new PlaneNode(size, colour, new SCNVector3(0.2f, bottomRowY, 0), 0.036f));
 
-            var geometry = SCNPlane.Create(width, length);
-            geometry.Materials = new[] { material };
-
-            var rootNode = new SCNNode();
-            rootNode.Geometry = geometry;
-
-            this.sceneView.Scene.RootNode.AddChildNode(rootNode);
+            // A circle would effectively be a square with corner radius of half the width (0.08f) so a corner radius of 0.04f
         }
-
         public override void ViewDidDisappear(bool animated)
         {
             base.ViewDidDisappear(animated);
@@ -70,6 +69,32 @@ namespace XamarinIosApp
         public override void DidReceiveMemoryWarning()
         {
             base.DidReceiveMemoryWarning();
+        }
+    }
+
+    public class PlaneNode : SCNNode
+    {
+        public PlaneNode(float size, UIColor color, SCNVector3 position, float cornerRadius)
+        {
+            var rootNode = new SCNNode
+            {
+                Geometry = CreateGeometry(size, color, cornerRadius),
+                Position = position
+            };
+
+            AddChildNode(rootNode);
+        }
+
+        private static SCNGeometry CreateGeometry(float size, UIColor color, float cornerRadius)
+        {
+            var material = new SCNMaterial();
+            material.Diffuse.Contents = color;
+
+            var geometry = SCNPlane.Create(size, size);
+            geometry.Materials = new[] { material };
+            geometry.CornerRadius = cornerRadius;
+
+            return geometry;
         }
     }
 }
