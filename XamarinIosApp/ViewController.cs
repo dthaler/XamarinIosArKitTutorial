09994aa379aa0baaec728505cef0b5eff9b322dc
Dave Thaler <dthaler@ntdev.microsoft.com>
2021-01-01 23:54:19 -0800

    Lesson 12: Show local image on plane
    Signed-off-by: Dave Thaler <dthaler@ntdev.microsoft.com>

--- XamarinIosApp/ViewController.cs
+++ XamarinIosApp/ViewController.cs
@@ -16,8 +16,7 @@ namespace XamarinIosApp
             this.sceneView = new ARSCNView
             {
                 AutoenablesDefaultLighting = true,
-                DebugOptions =
-                ARSCNDebugOptions.ShowFeaturePoints | ARSCNDebugOptions.ShowWorldOrigin
+                DebugOptions = ARSCNDebugOptions.ShowWorldOrigin
             };
 
             this.View.AddSubview(this.sceneView);
@@ -39,16 +38,26 @@ namespace XamarinIosApp
                 AutoFocusEnabled = true,
                 PlaneDetection = ARPlaneDetection.Horizontal,
                 LightEstimationEnabled = true,
-                WorldAlignment = ARWorldAlignment.Gravity
+                WorldAlignment = ARWorldAlignment.GravityAndHeading
             }, ARSessionRunOptions.ResetTracking | ARSessionRunOptions.RemoveExistingAnchors);
 
-            // Lesson: Add text to scene
-            var textNode = new TextNode("Hello Universe", UIColor.Orange)
-            {
-                Position = new SCNVector3(0, -0.6f, 0) // Any lower than this and it doesn't show!?
-            };
+            // Lesson: Add image to plane
+            var width = 0.1f;
+            var length = 0.1f;
 
-            this.sceneView.Scene.RootNode.AddChildNode(textNode);
+            var image = UIImage.FromFile("Images/pineapple.jpg");
+
+            var material = new SCNMaterial();
+            material.Diffuse.Contents = image;
+            material.DoubleSided = true;
+
+            var geometry = SCNPlane.Create(width, length);
+            geometry.Materials = new[] { material };
+
+            var rootNode = new SCNNode();
+            rootNode.Geometry = geometry;
+
+            this.sceneView.Scene.RootNode.AddChildNode(rootNode);
         }
 
         public override void ViewDidDisappear(bool animated)
@@ -61,31 +70,6 @@ namespace XamarinIosApp
         public override void DidReceiveMemoryWarning()
         {
             base.DidReceiveMemoryWarning();
-        }
-    }
-
-    public class TextNode : SCNNode
-    {
-        public TextNode(string text, UIColor color)
-        {
-            var rootNode = new SCNNode
-            {
-                Geometry = CreateGeometry(text, color),
-                Position = new SCNVector3(0, 0, 0)
-            };
-
-            AddChildNode(rootNode);
-        }
-
-        private static SCNGeometry CreateGeometry(string text, UIColor color)
-        {
-            var geometry = SCNText.Create(text, 0.01f);
-            geometry.Font = UIFont.FromName("Courier", 0.5f);
-            geometry.Flatness = 0;
-            geometry.FirstMaterial.DoubleSided = true;
-            geometry.FirstMaterial.Diffuse.Contents = color;
-            geometry.FirstMaterial.Specular.Contents = UIColor.Blue;
-            return geometry;
         }
     }
 }
