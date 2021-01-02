295c03aaca33820f62466135cad395fc08922b10
Dave Thaler <dthaler@ntdev.microsoft.com>
2021-01-01 21:48:06 -0800

    Lesson 2: Create Blank Scene
    Signed-off-by: Dave Thaler <dthaler@ntdev.microsoft.com>

--- XamarinIosApp/ViewController.cs
+++ XamarinIosApp/ViewController.cs
@@ -1,19 +1,48 @@
-﻿using Foundation;
+﻿using ARKit;
+using Foundation;
+using SceneKit;
 using System;
+using System.Linq;
 using UIKit;
 
 namespace XamarinIosApp
 {
     public partial class ViewController : UIViewController
     {
+        private readonly ARSCNView sceneView;
+
         public ViewController(IntPtr handle) : base(handle)
         {
+            // Lesson: Create Blank Scene
+            this.sceneView = new ARSCNView();
+            this.View.AddSubview(this.sceneView);
         }
 
         public override void ViewDidLoad()
         {
             base.ViewDidLoad();
+
             // Perform any additional setup after loading the view, typically from a nib.
+            this.sceneView.Frame = this.View.Frame;
+        }
+
+        public override void ViewDidAppear(bool animated)
+        {
+            base.ViewDidAppear(animated);
+
+            this.sceneView.Session.Run(new ARWorldTrackingConfiguration
+            {
+                AutoFocusEnabled = true,
+                LightEstimationEnabled = true,
+                WorldAlignment = ARWorldAlignment.Gravity
+            }, ARSessionRunOptions.ResetTracking | ARSessionRunOptions.RemoveExistingAnchors);
+        }
+
+        public override void ViewDidDisappear(bool animated)
+        {
+            base.ViewDidDisappear(animated);
+
+            this.sceneView.Session.Pause();
         }
 
         public override void DidReceiveMemoryWarning()
